f70bbe6633521072832b42df428a8678920334f4
Yadi Apriyadi <yadi.ict@gmail.com>
2023-03-31 10:54:55 +0700

    add col role for midlleware

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -16,7 +16,7 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if(!auth()->check() || auth()->user()->nama !== "Admin")
+        if(!auth()->check() || auth()->user()->role !== "Admin")
         {
             abort(403);
         }
